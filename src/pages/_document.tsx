25379170ac07087f0bec25c4c957b004c7e87c85
manojkiranti <manoj.kiranti@gmail.com>
2023-07-02 12:31:39 +0545

    seo page added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head >
-
-       
+      <Head >       
         <meta name="description" content="Check your SEE (SLC) Result 2080 with marksheet via NIC Asia Bank. Get updates on SEE Results, symbol numbers, and Marksheet." />
         <meta name="keywords" content="SEE Result 2079, See Results 2078, Marksheet, Symbol Number, NIC Asia Bank, View Exam Results, Class 10 SEE Result 2079, SEE Results status" />
         <meta name="google-site-verification" content="MBKqPndNjDDmehoWazN-_HnqOPiGBz3nipDVeAKfpNc" />
